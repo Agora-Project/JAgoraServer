d69f5a30ac2410dc6990b748420a831c36d2d6bf
anvilfolk <anvilfolk@ohmu.lan>
2013-10-19 17:52:59 -0400

    - Slightly more advanced version of things!

--- src/org/agora/server/JAgoraServer.java
+++ src/org/agora/server/JAgoraServer.java
@@ -1,10 +1,60 @@
 package org.agora.server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.agora.server.logging.Log;
+import org.agora.server.logging.FileLog;
+
 
 public class JAgoraServer {
+  
+  public static int SERVER_PORT = 1597;
 
+  public static String LOG_FILE = "/var/log/agorad.log";
+  public static String ERROR_FILE = "/var/log/agorad.err";
+  public static String DEBUG_FILE = "/var/log/agorad.dbg";
+  
+  
+  
+  protected int serverPort;
+  
+  protected ServerSocket socket;
+  
+  public JAgoraServer(int serverPort) {
+    this.serverPort = serverPort;
+  }
+  
+  public void startListening() {
+    try {
+      socket = new ServerSocket(serverPort);
+      Socket clientSocket = socket.accept();
+      InputStream is = clientSocket.getInputStream();
+      while(true) {
+        int inData = is.read();
+        if (inData < 0)
+          break;
+        clientSocket.getOutputStream().write(inData);
+        clientSocket.getOutputStream().flush();
+      }
+      clientSocket.close();
+    } catch (IOException e) {
+      Log.error("[JAgoraServer] Failure while listening to sockets.");
+      Log.error(e.toString());
+    }
+  }
+  
+  public static void InitLogging() {
+    Log.addLog(new FileLog(LOG_FILE, ERROR_FILE, DEBUG_FILE));
+  }
+  
   public static void main(String[] args) {
-    System.out.println("RUNNING");
-    while(true) ;
+    JAgoraServer.InitLogging();
+    Log.log("[JAgoraServer] starting.");
+    JAgoraServer jas = new JAgoraServer(JAgoraServer.SERVER_PORT);
+    while (true)
+      jas.startListening();
   }
 }
---
+++ src/org/agora/server/Options.java
@@ -0,0 +1,7 @@
+package org.agora.server;
+
+public class Options {
+  public static boolean LOG_MESSAGES = true;
+  public static boolean DEBUG_MESSAGES = false;
+  public static boolean ERROR_MESSAGES = true;
+}
