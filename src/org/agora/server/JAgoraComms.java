ea36cce9ed21fe3656e2a6461dbd4c7d67993861
João G. Martins <anvilfolk@gmail.com>
2013-10-22 23:27:12 -0400

    Finished up on serialisation/deserialisation. Needs testing.

--- src/org/agora/server/JAgoraComms.java
+++ src/org/agora/server/JAgoraComms.java
@@ -27,8 +27,7 @@ public class JAgoraComms {
     try {
       InputStream is = s.getInputStream();
       BSONDecoder bdec = new BasicBSONDecoder();
-      BSONObject bson = bdec.readObject(is);
-      return bson;
+      return bdec.readObject(is);
     } catch (IOException e) {
       Log.error("Could not read BSON object from socket " + s);
       Log.error(e.getMessage());
@@ -106,64 +105,64 @@ public class JAgoraComms {
     return bsonGraph;
   }
   
- // *************************
- // **** Deserialization ****
- // *************************
-  
+ /*
+ *************************
+ **** Deserialization ****
+ *************************
+ */
+
   public static NodeID deBSONiseNodeID(BSONObject bsonNodeID) {
-    NodeID result = new NodeID(
+    return new NodeID(
         (String)bsonNodeID.get("source"),
-        (int)bsonNodeID.get("id")
+        (Integer)bsonNodeID.get("id")
     );
-    return result; 
   }
   
   public static Node deBSONiseNode(BSONObject bsonNode) {
     NodeID nodeID = deBSONiseNodeID((BSONObject)bsonNode.get("id"));
     Node node = new Node(nodeID);
     
-    node.setPosterID((int)bsonNode.get("id"));
+    node.setPosterID((Integer)bsonNode.get("id"));
     node.setPosterName((String)bsonNode.get("posterName"));
     // TODO: this is gong to break
     node.setDate((Date)bsonNode.get("date"));
-    node.setAcceptability((int)bsonNode.get("acceptability"));
-    node.setThreadID((int)bsonNode.get("threadID"));
+    node.setAcceptability((Integer)bsonNode.get("acceptability"));
+    node.setThreadID((Integer)bsonNode.get("threadID"));
 
     return node;
   }
   
   public static Edge deBSONiseEdge(BSONObject bsonEdge, Graph graph) {
-    
-    
-    // TODO: Node origin = graph.getNodeByID()
-    
-    BSONObject bson = new BasicBSONObject();
-    bson.put("origin", BSONiseNodeID(edge.getOrigin().getID()));
-    bson.put("target", BSONiseNodeID(edge.getTarget().getID()));
-    return bson;
+    NodeID originID = deBSONiseNodeID((BSONObject)bsonEdge.get("origin"));
+    NodeID targetID = deBSONiseNodeID((BSONObject)bsonEdge.get("target"));
+
+
+    // Check whether the origin nodes are or are not in the graph.
+    // If they are not, simply add a node containing only an ID.
+    // That should be enough to ask for it in case it's interesting.
+    Node originNode = null;
+    Node targetNode = null;
+
+    if (graph.isInGraph(originID)) originNode = graph.getNodeByID(originID);
+    else                           originNode = new Node(originID);
+
+    if (graph.isInGraph(targetID)) targetNode = graph.getNodeByID(targetID);
+    else                           targetNode = new Node(targetID);
+
+    return new Edge(originNode, targetNode);
   }
   
-  public static Graph BSONiseGraph(BSONObject bsonGraph) {
-    BSONObject bsonGraph = new BasicBSONObject();
-    
-    // Add nodes.
-    BasicBSONList bsonNodeList = new BasicBSONList();
-    Node[] nodes = graph.getNodes();
-    for (int i = 0; i < nodes.length; i++) {
-      bsonNodeList.add(BSONiseNode(nodes[i]));
-    }
-    
-    bsonGraph.put("nodes", bsonNodeList);
-    
-    // Add edges.
-    BasicBSONList bsonEdgeList = new BasicBSONList();
-    for (Edge e: graph.edgeMap.values()) {
-      bsonNodeList.add(BSONiseEdge(e));
-    }
-    
-    bsonGraph.put("edges", bsonEdgeList);
-    
-    return bsonGraph;
+  public static Graph deBSONiseGraph(BSONObject bsonGraph) {
+    Graph graph = new Graph();
+    BasicBSONList nodes = (BasicBSONList)bsonGraph.get("nodes");
+    for (Object n: nodes)
+      graph.addNode(deBSONiseNode((BSONObject) n));
+
+    BasicBSONList edges = (BasicBSONList)bsonGraph.get("edges");
+    for (Object e : edges)
+      graph.addEdge(deBSONiseEdge((BSONObject) e, graph));
+
+    return graph;
   }
 }
 
