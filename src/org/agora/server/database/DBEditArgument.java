d4d31510d141d298f58293014e2e275fafc40d1b
Greg <greg@greg-GM5454E.(none)>
2014-06-07 21:50:20 -0600

    Added Editing functionality. Probably doesn't work properly yet, needs more testing.

---
+++ src/org/agora/server/database/DBEditArgument.java
@@ -0,0 +1,44 @@
+package org.agora.server.database;
+
+import java.io.ByteArrayInputStream;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import org.agora.graph.JAgoraNodeID;
+import org.agora.server.DatabaseConnection;
+import org.bson.BSONEncoder;
+import org.bson.BSONObject;
+import org.bson.BasicBSONEncoder;
+import org.bson.BasicBSONObject;
+
+/**
+ *
+ * @author greg
+ */
+public class DBEditArgument {
+    
+    protected static String EDIT_QUERY = "UPDATE Arguments SET content=(CONTENT) WHERE arg_id=(ARG_ID), user_id=(USER_ID) VALUES(?, ?, ?);";
+    
+    public static boolean editArgumentOnDB(BSONObject content, JAgoraNodeID nodeID, int userID, DatabaseConnection dbc) throws SQLException {
+        PreparedStatement ps = dbc.prepareStatement(EDIT_QUERY);
+        
+        BSONEncoder benc = new BasicBSONEncoder();
+        byte[] b = benc.encode(content); 
+    
+        ps.setBinaryStream(1, new ByteArrayInputStream(b));
+        
+        ps.setInt(2, nodeID.getLocalID());
+        
+        ps.setInt(3, userID);
+        
+        ps.addBatch();
+    
+        ps.executeBatch();
+        int[] res = ps.executeBatch();
+    
+        ps.close();
+        dbc.close();
+
+        return res[0] != 0;
+    }
+
+}
---
+++ src/org/agora/server/queries/EditArgumentResponder.java
@@ -0,0 +1,58 @@
+package org.agora.server.queries;
+
+import com.mongodb.DBDecoder;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.agora.graph.JAgoraNodeID;
+import org.agora.lib.BSONGraphDecoder;
+import org.agora.lib.IJAgoraLib;
+import org.agora.logging.Log;
+import org.agora.server.JAgoraServer;
+import org.agora.server.database.DBEditArgument;
+import org.bson.BSONObject;
+import org.bson.BasicBSONObject;
+
+/**
+ *
+ * @author greg
+ */
+public class EditArgumentResponder implements QueryResponder {
+
+    @Override
+    public BasicBSONObject respond(BasicBSONObject query, JAgoraServer server) {
+        BasicBSONObject bsonResponse = new BasicBSONObject();
+        
+        boolean verified = server.verifySession(query);
+    
+        if (!verified) {
+            bsonResponse.put(IJAgoraLib.RESPONSE_FIELD, IJAgoraLib.SERVER_FAIL);
+            bsonResponse.put(IJAgoraLib.REASON_FIELD, "Invalid session ID.");
+            return bsonResponse;
+        }
+        
+        try {
+            BSONObject content = (BSONObject)query.get(IJAgoraLib.CONTENT_FIELD);
+            int userID = query.getInt(IJAgoraLib.USER_ID_FIELD);
+            JAgoraNodeID nodeID = new BSONGraphDecoder().deBSONiseNodeID((BasicBSONObject)query.get(IJAgoraLib.ARGUMENT_ID_FIELD));
+            
+            boolean res = DBEditArgument.editArgumentOnDB(content, nodeID, userID, server.createDatabaseConnection());
+            if (res) {
+                bsonResponse.put(IJAgoraLib.RESPONSE_FIELD, IJAgoraLib.SERVER_OK);
+            } else {
+                bsonResponse.put(IJAgoraLib.RESPONSE_FIELD, IJAgoraLib.SERVER_FAIL);
+                bsonResponse.put(IJAgoraLib.REASON_FIELD, "Attack already existed (with high probability!)");
+            }
+      
+            return bsonResponse;
+        } catch (SQLException e) {
+            Log.error("[EditArgumentResponder] Could not execute edit argument query ("+e.getMessage()+")");
+            bsonResponse.put(IJAgoraLib.RESPONSE_FIELD, IJAgoraLib.SERVER_FAIL);
+            bsonResponse.put(IJAgoraLib.REASON_FIELD, "Server failure.");
+            return bsonResponse;
+        } finally {
+            
+        }
+    }
+
+}
