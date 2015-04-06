b144a2d6a3b6d20b4097900ad47ae8de0c20a6bf
Angular-Angel <ribbonwing@gmail.com>
2015-04-06 12:06:27 -0600

    Added License.

--- src/org/agora/server/JAgoraServer.java
+++ src/org/agora/server/JAgoraServer.java
@@ -1,15 +1,28 @@
+/*
+
+Copyright (C) 2015 Agora Communication Corporation
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 package org.agora.server;
 
-import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.*;
 import java.util.concurrent.*;
 
 import org.agora.server.queries.*;
-import org.agora.lib.*;
-import org.agora.logging.ConsoleLog;
-import org.agora.logging.Log;
 import org.bson.BasicBSONObject;
 
 public interface JAgoraServer {
